f44ee0de6b6c3e6c5c7938c3b254ec892ced1185
BlackKubIsI <xatakekakasi170@gmail.com>
2022-02-04 19:45:26 +0300

    part 1 (2 commit)

--- maps_ip.py
+++ maps_ip.py
@@ -2,10 +2,11 @@ import requests
 from PyQt5.QtGui import QPixmap
 MAP_API_SERVER = "http://static-maps.yandex.ru/1.x/"
 
-def get_map_image(center_pos, map_size, map_type="map"):
+def get_map_image(center_pos, map_size, map_type="map", size=(500, 450)):
     params = {"ll": f"{center_pos[0]},{center_pos[1]}",
               "l": map_type,
-              "spn": f"{map_size[0]},{map_size[1]}"}
+              "spn": f"{map_size[0]},{map_size[1]}",
+              "size": f"{size[0]},{size[1]}"}
     response = requests.get(MAP_API_SERVER, params=params).content
     map = QPixmap()
     map.loadFromData(response)
--- part_1.py
+++ part_1.py
@@ -6,12 +6,12 @@ from maps_ip import get_map_image
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(200, 200, 500, 500)
+        self.setGeometry(200, 200, 500, 450)
         self.settings()
     
     def settings(self):
         self.map = QLabel(self)
-        self.map.setGeometry(0, 0, 500, 500)
+        self.map.setGeometry(0, 0, 500, 450)
         self.map.setPixmap(get_map_image((37.6156, 55.7522), (0.1, 0.1)))
 
 app = QApplication(sys.argv)
